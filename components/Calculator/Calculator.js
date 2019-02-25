c281ac895270bd6547cc549e0b4398dddfadc670
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-02-25 15:58:05 +0600

    add Calculator component

---
+++ components/Calculator/Calculator.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {StyleSheet, Text, TouchableHighlight,} from "react-native";
+
+export default class Calculator extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    onPress = () => {
+        const {onPress, title} = this.props;
+        if (onPress) {
+            onPress(title);
+        }
+    };
+
+    render() {
+        return (
+            <TouchableHighlight
+                onPress={this.onPress}
+                style={[styles.container, this.props.style]}
+                underlayColor = {this.props.underlayColor || '#d1d1d1'}>
+                <Text style = {[styles.title, this.props.titleStyle]}>
+                    {this.props.title}
+                </Text>
+            </TouchableHighlight>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    title: {
+        fontSize: 25,
+    }
+});
