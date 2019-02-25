cd8ff82a1df54e72064bba26e53985f36973e679
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-02-25 16:01:54 +0600

    add CalcBuilder container

--- App.js
+++ App.js
@@ -1,33 +1,10 @@
 import React from 'react';
-import {StyleSheet, TextInput, View} from 'react-native';
+import CalcBuilder from "./containers/CalcBuilder/CalcBuilder";
 
 export default class App extends React.Component {
-  state = {
-    placeName: ''
-  };
-
-  placeNameChangeHandler = value => {
-    this.setState({placeName: value})
-  };
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <TextInput
-            value={this.state.placeName}
-            style={{width: 300, borderColor: 'black', borderWidth: 1}}
-            onClick={this.placeNameChangeHandler}
-        />
-      </View>
-    );
-  }
+    render() {
+        return (
+            <CalcBuilder/>
+        );
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
